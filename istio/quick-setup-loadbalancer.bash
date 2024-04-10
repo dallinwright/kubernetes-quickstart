6b3f23a0b9456e42f24ac29251a79d85bd14e60a
Dallin Wright <dallinwright01@gmail.com>
2024-04-09 17:13:21 -0700

    small fix

--- istio/quick-setup-loadbalancer.bash
+++ istio/quick-setup-loadbalancer.bash
@@ -6,6 +6,6 @@ kubectl config set-context --current --namespace=istio-ingress
 helm upgrade --install istio-ingressgateway istio/gateway -n istio-ingress --set service.type=LoadBalancer
 
 kubectl apply -f istio/gateway.yaml
-kubectl apply -f istio/virtual_service.yaml
+kubectl apply -f istio/virtual-service.yaml
 
 kubectl describe gateway -n istio-ingress && kubectl get virtualservice -n istio-ingress
