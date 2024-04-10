a0759597f95d0ca9af2ac92150e521be95fdc5cd
Dallin Wright <dallinwright@pm.me>
2024-04-10 15:51:19 -0700

    working

--- knative/setup.bash
+++ knative/setup.bash
@@ -14,22 +14,33 @@ kubectl label namespace knative-serving istio-injection=enabled --overwrite
 kubectl wait --for=condition=Established --timeout=60s crd/knativeservings.operator.knative.dev
 kubectl wait --for=condition=Established --timeout=60s crd/peerauthentications.security.istio.io
 
+echo "Setting up knative-serving"
 kubectl apply -f knative/serving.yaml
 
+echo "Setting up istio network plugin CRDs"
 kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.13.1/net-istio.yaml
+
+sleep 30
 
 kubectl wait --for=condition=Established --timeout=60s crd/certificates.networking.internal.knative.dev
 kubectl wait --for=condition=Established --timeout=60s crd/configurations.serving.knative.dev
 
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.13.1/serving-default-domain.yaml
+echo "Setting up configmap for domain mapping"
 kubectl patch configmap/config-domain \
   --namespace knative-serving \
   --type merge \
   --patch '{"data":{"example.com":""}}'
 
-kubectl patch configmap config-istio -n knative-serving \
-  --type merge \
-  -p '{"data":{"gateway.knative-serving.knative-ingress-gateway":"istio-ingressgateway.istio-ingress.svc.cluster.local"}}'
+echo "Setting up knative configmap for istio gateway"
+# Fetch the current configmap and save it to a file
+kubectl get configmap config-istio -n knative-serving -o json > config-istio.json
+
+# Use jq to replace the specific key-value pair in the data field
+jq '.data["gateway.knative-serving.knative-ingress-gateway"]="istio-ingressgateway.istio-ingress.svc.cluster.local"' config-istio.json > config-istio-updated.json
+
+# Apply the updated configmap
+kubectl apply -f config-istio-updated.json
 
 # Peering for Istio + Cert Manager for Knative, still TODO
+echo "Setting up peering for Istio + Cert Manager for Knative"
 kubectl apply -f knative/peering.yaml
