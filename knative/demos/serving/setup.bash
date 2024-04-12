14d40ad789369fc1ab9730a7d0d1b84a458cbfcd
Dallin Wright <dallinwright@pm.me>
2024-04-12 09:39:59 -0700

    setup a private registry credential helper, if needed

--- knative/demos/serving/setup.bash
+++ knative/demos/serving/setup.bash
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
 
-#cd knative/demos/serving/hello-world || exit
-#
-## Check if Docker is running
-#if ! docker info >/dev/null 2>&1; then
-#    echo "Docker does not seem to be running, start Docker before running this script."
-#    exit 1
-#fi
-#
-## Check if the builder "container" exists
-#if ! docker buildx ls | grep -q 'container'; then
-#    # Create the builder
-#    docker buildx create --name container --driver docker-container --use
-#else
-#    # Use the existing builder
-#    docker buildx use container
-#fi
-#
-#docker buildx build --platform linux/amd64,linux/arm64 --builder=container -t "dallinpm/hello-world:latest" --push .
-#
-#cd ../../../.. || exit
-
 # Our services namespace
 kubectl create namespace services
 kubectl config set-context --current --namespace=services
@@ -30,7 +9,3 @@ kubectl apply -f knative/demos/serving/knative-service.yaml
 
 kubectl get ksvc hello-world
 kubectl get svc istio-ingressgateway -n istio-ingress
-
-# shellcheck disable=SC2046
-#curl $(kn service describe hello-world -o url)
-kubectl patch configmap config-istio -n knative-serving --type json -p '[{"op": "remove", "path": "/data/local-gateway.istio-ingress.knative-local-gateway"}]'
--- knative/setup.bash
+++ knative/setup.bash
@@ -12,32 +12,12 @@ kubectl config set-context --current --namespace=knative-serving
 kubectl label namespace knative-serving istio-injection=enabled --overwrite
 
 kubectl wait --for=condition=Established --timeout=60s crd/knativeservings.operator.knative.dev
-kubectl wait --for=condition=Established --timeout=60s crd/peerauthentications.security.istio.io
 
 echo "Setting up knative-serving"
 kubectl apply -f knative/serving.yaml
 
-sleep 10
-
 echo "Setting up istio network plugin CRDs"
 kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.13.1/net-istio.yaml
-
-sleep 10
-
-echo "Setting up configmap for domain mapping"
-kubectl patch configmap/config-domain \
-  --namespace knative-serving \
-  --type merge \
-  --patch '{"data":{"example.com":""}}'
-
-sleep 15
-
-# Delete the specific key from the configmap
-kubectl patch configmap config-istio -n knative-serving --type json -p '[{"op": "remove", "path": "/data/local-gateway.istio-ingress.knative-local-gateway"}]'
-
-# Add the new key-value pair to the configmap
-kubectl patch configmap config-istio -n knative-serving --type merge -p '{"data":{"gateway.knative-serving.knative-ingress-gateway":"istio-ingressgateway.istio-ingress.svc.cluster.local"}}'
-kubectl get configmap config-istio -n knative-serving -o yaml
 
 # Peering for Istio + Cert Manager for Knative, still TODO
 echo "Setting up peering for Istio + Cert Manager for Knative"
